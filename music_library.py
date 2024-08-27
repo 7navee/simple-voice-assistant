d19fc6107a03eeb1bd1d55b9795f6925aac475b3
Naveeen kumar H B <106253665+7navee@users.noreply.github.com>
2024-08-27 12:10:50 +0530

    Update music_library.py

--- music_library.py
+++ music_library.py
@@ -1,5 +1,5 @@
 music = {
-    "cat" : "https://youtu.be/wiur_AGatGU",
-    "ball" : "https://youtu.be/mEFn-MK3jyM",
-    "chat": "https://youtu.be/hUxOFasdZQI?list=RDhUxOFasdZQI"
+    "song1" : "https://youtu.be/wiur_AGatGU",
+    "song2" : "https://youtu.be/mEFn-MK3jyM",
+    "song3": "https://youtu.be/hUxOFasdZQI?list=RDhUxOFasdZQI"
 }
